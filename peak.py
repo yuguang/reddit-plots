1100fb3d476079cec9d8474be7694d1875d3b8aa
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-22 20:44:50 -0400

    add script with query for peak hours and days of week

---
+++ peak.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from connect import engine
+import json
+
+# noinspection SqlDerivedTableAlias
+peak_hour_post_by_subreddit = pd.read_sql_query("""
+SELECT subreddit, hour, COUNT(created_utc) as count
+FROM (
+    -- truncate dates into hours
+    SELECT subreddit, DATE_PART(h, created_utc) as hour, created_utc, score
+    FROM comment_timestamps
+        -- select the most popular subreddits
+        WHERE subreddit IN (
+            SELECT subreddit
+            FROM reddit_comments
+            GROUP BY subreddit HAVING COUNT(created_utc) > 1000
+        ) ORDER BY subreddit
+    )
+WHERE score > 1000
+-- bubcket into subreddits and hours
+GROUP BY subreddit, hour
+ORDER BY count DESC, subreddit, hour
+;""", engine)
+
+# noinspection SqlDerivedTableAlias
+peak_weekday_post_by_subreddit = pd.read_sql_query("""
+SELECT subreddit, day, COUNT(created_utc) as count
+FROM (
+    -- truncate dates into days
+    SELECT subreddit, DATE_PART(dow, created_utc) as day, created_utc, score
+    FROM comment_timestamps
+        -- select the most popular subreddits
+        WHERE subreddit IN (
+            SELECT subreddit
+            FROM reddit_comments
+            GROUP BY subreddit HAVING COUNT(created_utc) > 1000
+        ) ORDER BY subreddit
+    )
+WHERE score > 1000
+-- bubcket into subreddits and days
+GROUP BY subreddit, day
+ORDER BY count DESC, subreddit, day
+;""", engine)
+
+output = []
+total = peak_hour_post_by_subreddit.groupby('subreddit')['count'].sum()
+for line in peak_hour_post_by_subreddit.sort(['subreddit', 'hour']).values:
+    subreddit, hour, count = line
+    if total[subreddit] < 300:
+        continue
+    percent = float(count/float(total[subreddit]))
+    # convert hour to PST
+    output.append('\t'.join(map(lambda x: str(x), [subreddit, (int(hour) + 5)%24, percent, '\n'])))
+with open('peak.tsv', 'w') as outfile:
+    outfile.writelines(output)
+
+weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+output = []
+total = peak_weekday_post_by_subreddit.groupby('subreddit')['count'].sum()
+for line in peak_weekday_post_by_subreddit.sort(['subreddit', 'day']).values:
+    subreddit, day, count = line
+    if total[subreddit] < 300:
+        continue
+    percent = float(count/float(total[subreddit]))
+    output.append('\t'.join(map(lambda x: str(x), [subreddit, weekdays[int(day)], percent, '\n'])))
+with open('peak.tsv', 'w') as outfile:
+    outfile.writelines(output)
