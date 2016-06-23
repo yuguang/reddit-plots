0197fd08d3e84312536aef8b5d934602cc17e44a
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-06-23 14:54:41 -0400

    add query to show the median comment length by subreddit

--- score.py
+++ score.py
@@ -24,3 +24,23 @@ FROM reddit_comments WHERE count < 2000) GROUP BY comment_length;""", engine)
 # plot in high charts
 for line in df.sort_values(by='comment_length').values:
     print("[", line[0], ",", line[1], ",", line[3], ",", line[4], ",", line[5], ",", line[2], "],")
+
+# comment length by subreddit
+length_by_subreddit = pd.read_sql_query("""SELECT MIN(median) as length, subreddit FROM (
+SELECT subreddit, author, MEDIAN(count)
+OVER (PARTITION BY subreddit)
+FROM reddit_comments
+) GROUP BY subreddit  HAVING COUNT(DISTINCT author) > 500
+ORDER BY length DESC;""", engine)
+
+df = length_by_subreddit[:500]
+
+data = [
+    Bar(
+        x=df.subreddit,
+        y=df.length
+    )
+]
+
+layout = Layout(title="Comment Length by Subreddit", yaxis=YAxis(title="Median Comment Length (in words)"))
+plot_url = py.plot(Figure(data=data, layout=layout))
