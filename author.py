64cb5a41553348c885c395048bf94553f6c2a093
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-06-23 15:43:34 -0400

    add graphs for upvotes

--- author.py
+++ author.py
@@ -22,3 +22,42 @@ data = [
 ]
 layout = Layout(title="Unique Gilded Authors by Subreddit", yaxis=YAxis(title="Gilded Authors from 2007 - 2015"))
 plot_url = py.plot(Figure(data=data, layout=layout))
+
+# number of authors with scores >1k by subreddit
+upvoted_authors_by_subreddit = pd.read_sql_query("""
+    SELECT subreddit, COUNT(DISTINCT author) as authors
+    FROM reddit_comments
+    GROUP BY subreddit
+    HAVING SUM(ups) > 1000
+    ORDER BY authors DESC;
+    """, engine)
+
+df = upvoted_authors_by_subreddit[0:100]
+data = [
+    Bar(
+        x=df.subreddit,
+        y=df.authors
+    )
+]
+layout = Layout(title="Unique Upvoted Authors by Subreddit", yaxis=YAxis(title="Upvoted Authors from 2007 - 2015 (with > 1k total upvotes)"))
+plot_url = py.plot(Figure(data=data, layout=layout))
+
+# histogram of upvotes
+upvote_frequency = pd.read_sql_query("""
+    SELECT ups::INT, COUNT(created_utc) as frequency
+    FROM reddit_comments
+    WHERE ups > 0
+    GROUP BY ups
+    ORDER BY ups ASC;
+    """, engine)
+
+df = upvote_frequency[0:100]
+data = [
+    Bar(
+        x=df.ups,
+        y=df.frequency
+    )
+]
+layout = Layout(title="Upvotes for Reddit submissions from 2007 - 2015", yaxis=YAxis(title="Frequency"), xaxis=XAxis(title="Upvotes"))
+plot_url = py.plot(Figure(data=data, layout=layout))
+
